16617d1de20204c1a6b3200feaf6ae7a801e4999
ryojiagatsuma1004 <agatsuma.ryoji.as9@naist.ac.jp>
2024-04-25 16:49:40 +0900

    fix get file path method

--- test/test_CSVPrinter.py
+++ test/test_CSVPrinter.py
@@ -1,20 +1,25 @@
 import unittest
 from speciallecturetest.CSVPrinter import CSVPrinter
+import os
 
 
 class TestCSVPrinter(unittest.TestCase):
     def test_read_is_row_collect(self):
-        printer = CSVPrinter("sample.csv")
+        file_path = os.path.join(os.path.dirname(__file__), "sample.csv")
+        printer = CSVPrinter(file_path)
         l = printer.read()
         self.assertEqual(3, len(l))
         # self.assertEqual(30, len(l))
 
     def test_read_is_colum_connect(self):
-        printer = CSVPrinter("sample.csv")
+        file_path = os.path.join(os.path.dirname(__file__), "sample.csv")
+        printer = CSVPrinter(file_path)
         l = printer.read()
         self.assertEqual(3, len(l[0]))
 
     def test_read_when_file_is_not_exist_throw_error(self):
-        printer = CSVPrinter("sample2.csv")
+        file_path = os.path.join(os.path.dirname(__file__), "sample.csv")
+        file_path = file_path + ".notExits"
+        printer = CSVPrinter(file_path)
         with self.assertRaises(Exception):
             l = printer.read()
